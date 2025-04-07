fbf32328cdc38040fd27dedeb644844b23cf99a9
Tyler Poirrier <58955432+TheTiller022@users.noreply.github.com>
2025-04-07 22:54:35 +0000

    corrected naming conventions

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # main calls information from input and libaries and thus need to me imported
-import Input.input_module.py
-import Lib.Class1.py
-import Lib.Class2.py
+import Input.input_module
+import Lib.DataFrame
+import Lib.Sales_Calculation
+
 
 
 
