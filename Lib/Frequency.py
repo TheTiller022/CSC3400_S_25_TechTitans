8bb12b969d5f112cca3f90a6b31675805e7e5f78
tylerguidry07 <tyler.guidry-2@selu.edu>
2025-04-07 19:49:30 -0500

    Frequency.py

--- Lib/Frequency.py
+++ Lib/Frequency.py
@@ -1,4 +1,11 @@
 # Parent class 1
+'''
+this class will measure the frequency of each flavor bought, regardless of size or topping
+    - we will so this using a histogram to find the distribution/frewuency differences between the different flavors
+
+to query this data for searching, we will read our data from the csv file with all of the order information on it
+    - this will allow us to scan for each instance of a particular flavor and keep track of how many times they appear
+'''
 
 def test():
     print("hi")
