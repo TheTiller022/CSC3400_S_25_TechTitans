06de86b20cc90d3f02e95f0e0524196dd5401b8c
tylerguidry07 <tyler.guidry-2@selu.edu>
2025-04-07 20:14:11 -0500

    Sales.py

--- Lib/Sales.py
+++ Lib/Sales.py
@@ -1,1 +1,10 @@
 # Parent class 2
+'''
+the second parent class, Sales, will define the utility functions used by the child class
+  - mean will calculate the average apperances of a desired element
+  - median will calculate the middle value of numerical data such as price
+    - after the data is sorted
+  - mode will calculate which of the desired variation information appears the most often
+  - the standard deviation (std) will find the value of how far the data is from the mean (average)
+
+'''
